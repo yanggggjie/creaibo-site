1f5deafc1735b101b8f9ad25b8f0b1441e5ee15e
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-12-12 18:46:55 +0800

    init

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': 'off',
+      'no-empty-pattern': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'prefer-const': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-extra-semi': 'off',
+      '@typescript-eslint/ no-empty-object-type': 'off',
+    },
+  },
+)
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+interface Props {}
+
+export default function App({}: Props) {
+  return (
+    <div>
+      <div>hello world</div>
+    </div>
+  )
+}
